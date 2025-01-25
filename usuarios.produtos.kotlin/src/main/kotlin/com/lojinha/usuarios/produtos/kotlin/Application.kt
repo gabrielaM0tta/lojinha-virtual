49a30779abe39a8515a3ddce8e4be3007851d14e
Gabriela S Motta <gbss.santos@poli.ufrj.br>
2025-01-25 14:34:09 -0300

    bug: o serviçode banco sobe mas o gradle esta dando problema

---
+++ usuarios.produtos.kotlin/src/main/kotlin/com/lojinha/usuarios/produtos/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package com.lojinha.usuarios.produtos.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
